4023c9c150d91beb589ce1211b9f95f6afe2ebe3
blystay-ux <blystay@gmail.com>
2026-05-18 22:27:51 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,27 @@
+import { Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './contexts/AuthContext'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Search from './pages/Search'
+import HotelDetail from './pages/HotelDetail'
+import Auth from './pages/Auth'
+import ListHotel from './pages/ListHotel'
+import Admin from './pages/Admin'
+import ManageHotel from './pages/ManageHotel'
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/hotel/:slug" element={<HotelDetail />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/list-hotel" element={<ListHotel />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/manage-hotel" element={<ManageHotel />} />
+      </Routes>
+    </AuthProvider>
+  )
+}
