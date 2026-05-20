71381867dfd35fc3b2f7483067c126ca1dc0f737
blystay-ux <blystay@gmail.com>
2026-05-20 08:08:53 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, useLocation } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
@@ -8,31 +8,24 @@ import Auth from './pages/Auth'
 import ListHotel from './pages/ListHotel'
 import Admin from './pages/Admin'
 import ManageHotel from './pages/ManageHotel'
-
-function Layout() {
-  const location = useLocation()
-  const hideNav = location.pathname === '/'
-
-  return (
-    <>
-      {!hideNav && <Navbar />}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/hotel/:slug" element={<HotelDetail />} />
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/list-hotel" element={<ListHotel />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/manage-hotel" element={<ManageHotel />} />
-      </Routes>
-    </>
-  )
-}
+import MyBookings from './pages/MyBookings'
+import Extranet from './pages/Extranet'
 
 export default function App() {
   return (
     <AuthProvider>
-      <Layout />
+      <Navbar />
+      <Routes>
+        <Route path="/"              element={<Home />} />
+        <Route path="/search"        element={<Search />} />
+        <Route path="/hotel/:slug"   element={<HotelDetail />} />
+        <Route path="/auth"          element={<Auth />} />
+        <Route path="/list-hotel"    element={<ListHotel />} />
+        <Route path="/admin"         element={<Admin />} />
+        <Route path="/manage-hotel"  element={<ManageHotel />} />
+        <Route path="/my-bookings"   element={<MyBookings />} />
+        <Route path="/extranet"      element={<Extranet />} />
+      </Routes>
     </AuthProvider>
   )
 }
